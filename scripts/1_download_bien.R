dc7c9959e7a99c3ad2151dae651ea70088e8ca78
ChrKoenig <chr.koenig@outlook.com>
2021-02-16 11:26:03 +0100

    updated download scripts for bien and gbif data

--- scripts/1_download_bien.R
+++ scripts/1_download_bien.R
@@ -7,7 +7,7 @@ setwd("~/ownCloud/Projects/Berlin/10_Pacific_invaders")
 inv_df = read.csv("data/Pacific_Invaders_GIFT_22_01.csv", sep = ";")
 inv_specs = unique(inv_df$Species) 
 
-download_bien = mclapply(inv_specs[1:8], mc.cores = 4, function(x){
+download_bien = mclapply(inv_specs, mc.cores = 6, function(x){
   download_block = BIEN_occurrence_species(x, native.status = T, political.boundaries = T)
   if(nrow(download_block) == 0){
     return(NULL)
--- scripts/1_download_gbif.R
+++ scripts/1_download_gbif.R
@@ -1,23 +1,27 @@
 library(tidyverse)
 library(taxize)
 library(rgbif)
-library(parallel)
+library(foreach)
+library(doParallel)
 
+rm(list = ls())
 setwd("~/ownCloud/Projects/Berlin/10_Pacific_invaders")
-###########################
-inv_df = read.csv("data/Pacific_Invaders_GIFT_22_01.csv", sep = ";")
-inv_specs = unique(inv_df$Species) 
 
-download_gbif = mclapply(inv_specs, mc.cores = 4, function(x){
-  gbif_id = taxize::get_gbifid(x, rank = "species", rows = 1, phylum = "Tracheophyta") %>% as.character()
-  if(is.na(gbif_id)){return(NA)}
-  
+###########################
+# Define download function
+download_species = function(spec_name){
+  # Prepare download
+  gbif_id = taxize::get_gbifid(spec_name, rank = "species", rows = 1, phylum = "Tracheophyta") %>% as.character()
+  if(is.na(gbif_id)){
+    return(NA)
+  }
   n_occ = rgbif::occ_count(taxonKey = gbif_id, georeferenced = T)
-  n_blocks = ceiling(n_occ / 100000) # Maximum download size of 100.000 occurrences -> create chunks
+  n_blocks = ceiling(n_occ / 50000) # Maximum download size of 100.000 occurrences -> create chunks with safety margins for uneven latitudinal distribution (50000 occ)
   lat_ranges = str_replace_all(levels(cut_interval(-90:90, n_blocks)), "\\[|\\]|\\(|\\)", "") # create latitudinal bands for blocked download
   
-  download_list = lapply(lat_ranges, FUN = function(lat_range){
-    download_block = occ_data(scientificName = x, hasCoordinate = T, limit = 99999, decimalLatitude = lat_range)$data 
+  # Download data in latitudinal bands, retry of not successful
+  download_list = lapply(lat_ranges, FUN = function(lat_range){  
+    download_block = rgbif::occ_data(scientificName = spec_name, hasCoordinate = T, limit = 100000, decimalLatitude = lat_range)$data
     if(is.null(download_block)){
       return(NULL)
     } else {
@@ -26,8 +30,34 @@ download_gbif = mclapply(inv_specs, mc.cores = 4, function(x){
                                                              "countryCode", "country")])
     }
   }) 
-  occ_df = bind_rows(download_list) %>% 
-    distinct()
-})
+  occ_df = bind_rows(download_list) %>% distinct()
+  return(occ_df)
+}
 
-save(download_gbif, file = "data/download_gbif.RData")
+# Loop over species and download occurrences
+cl = makeCluster(7)
+registerDoParallel(cl)
+
+inv_df = read.csv("data/Pacific_Invaders_GIFT_22_01.csv", sep = ";")
+inv_specs = unique(inv_df$Species) 
+inv_specs_dl = list.files("data/gbif_download/") %>% str_remove(".RData") %>% str_replace("_", " ")
+inv_specs_final = setdiff(inv_specs, inv_specs_dl)
+foreach(spec_name = inv_specs_final, .packages = c("tidyverse", "taxize", "rgbif")) %dopar% {
+  download_successful = F
+  iter = 0
+  occ_df = NULL
+  while(!(download_successful) & iter < 5){
+    cat(download_successful, iter, "\n")
+    tryCatch({
+      occ_df = download_species(spec_name)
+      download_successful = T
+    }, error = function(e){
+      cat("download error:", spec_name, "\n")
+      Sys.sleep(15)
+      iter <<- iter + 1
+    })
+  }  
+  save(occ_df, file = paste0("data/gbif_download/", str_replace_all(spec_name, " ", "_"), ".RData"))
+}  
+
+stopCluster(cl)
