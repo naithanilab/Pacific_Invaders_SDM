061b7a3ad05808bdde45374c07f9b5642fe7b0bd
Christian König <koenig@binfcalc9z.bio.uni-potsdam.de>
2021-04-29 16:04:53 +0200

    new branch for calc9

--- scripts/5_data_exploration.R
+++ scripts/5_data_exploration.R
@@ -3,24 +3,28 @@ library(sf)
 library(maps)
 
 rm(list = ls())
-setwd("~/ownCloud/Projects/Berlin/10_Pacific_invaders") #setwd("~/PacificInvadersSDM/")
+#setwd("~/ownCloud/Projects/Berlin/10_Pacific_invaders") 
+setwd("~/PacificInvadersSDM/")
 source("scripts/utils.R")
 
 ########## Load data ##########
-# load("data/occ_status_final.RData") #load("/./import/calc9z/data-zurell/koenig/occ_status_final.RData")
-# load("data/occ_cleaned_slim.RData") #load("/./import/calc9z/data-zurell/koenig/occ_cleaned_slim.RData")
-# occ = occ_cleaned_slim %>%
-#   dplyr::select(-dataset, -native) %>%
-#   left_join(occ_status_final, by = "occ_id") %>%
-#   mutate(status = replace_na(status, "unknown"))
-# rm(occ_status_final, occ_cleaned_slim)
-# save(occ, file = "data/occ.RData")
+#load("data/occ_status_final.RData") 
+load("/./import/calc9z/data-zurell/koenig/occ_status_final.RData")
+#load("data/occ_cleaned_slim.RData") 
+load("/./import/calc9z/data-zurell/koenig/occ_cleaned_slim.RData")
+occ = occ_cleaned_slim %>%
+  dplyr::select(-dataset, -native) %>%
+  left_join(occ_status_final, by = "occ_id") %>%
+  mutate(status = replace_na(status, "unknown"))
+rm(occ_status_final, occ_cleaned_slim)
+#save(occ, file = "data/occ.RData")
+save(occ, file = "/./import/calc9z/data-zurell/koenig/occ.RData")
 
-load("data/occ.RData")
+load("/./import/calc9z/data-zurell/koenig/occ.RData")
 pac_mask = st_read("data/pacific/pac_final.shp")
 geoentities = st_read("data/geoentities_simple_2019-08-29/geoentities_simple.shp")
 world = map_data("world")
-inv_specs = read.csv("data/Pacific_Invaders_GIFT_22_01.csv", sep = ";") %>% pull(Species) %>% unique()
+inv_specs = unique(occ$species)
 
 ########## Explore global patterns ##########
 # Frequency of status assignments
@@ -54,10 +58,12 @@ ggsave("plots/status_suspect/PlaSti.png", plot_status("Platymiscium stipulare"),
 ########## Explore pacific islands ############
 # Occ / islands * species
 # occ_pac_sf = occ %>% # This requires a lot of RAM to be fast!
-#   st_as_sf(coords = c("lon", "lat"), crs = 4326) %>% 
+#   st_as_sf(coords = c("lon", "lat"), crs = 4326) %>%
 #   st_join(pac_mask, left = F)
 # save(occ_pac_sf, file = "data/occ_pac_sf.RData")
-load("data/occ_pac_sf.RData")
+# load("data/occ_pac_sf.RData")
+load("/./import/calc9z/data-zurell/koenig/occ_pac_sf.RData")
+
 occ_pac = occ_pac_sf %>% 
   dplyr::mutate(lat = sf::st_coordinates(.)[,2], lon = sf::st_coordinates(.)[,1]) %>% 
   st_drop_geometry() %>% 
@@ -72,7 +78,6 @@ ggsave("plots/status_Samoa.png", plot_status(bbox = c(-173,-14.5,-169,-13), titl
 ggsave("plots/status_Vanuatu.png", plot_status(bbox = c(166.25,-20.35,170,-13.5), title = "Vanuatu"), scale = 1.5) # Vanuatu
 ggsave("plots/status_Solomons.png", plot_status(bbox = c(154.5,-11,162.5,-5), title = "Solomons"), scale = 1.5) # Solomons
 ggsave("plots/status_Kiribati.png", plot_status(bbox = c(-157.6,1.6,-157,2.1), title = "Kiribati"), scale = 1.5) # Kiribati
-
 
 ########## Explore Hawaii ############
 occ_hawaii = occ_pac_sf %>% 
@@ -109,4 +114,3 @@ ranking = decision_table %>%
   filter(region == "hawaii") %>% 
   mutate(total_non_native = `non-native` + naturalized + invasive) %>% 
   arrange(desc(usda_weed), desc(total_non_native))
-  
